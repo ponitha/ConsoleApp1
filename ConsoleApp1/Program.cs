f51b6e253c3e87ad2018c48381880dfe1fd27615
Narlangala <Ponitha.Narlangala@thomsonreuters.com>
2018-10-04 20:57:41 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder("Hello ", 50);
+
+            sb.Append("World!!");
+            sb.AppendLine("Hello C#!");
+            sb.AppendLine("This is new line.");
+
+            Console.WriteLine(sb);
+        }
+    }
+}
